8109c0985075a0622bd39f35c26f961f5d1ab7db
Alex Popkov <poalrom@yandex-team.ru>
2022-11-23 15:44:43 +0300

    react-redux-cart

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from "redux";
+import { productsReducer } from "./productsReducer.js";
+
+export const store = createStore(
+    combineReducers({
+        products: productsReducer
+    })
+)
